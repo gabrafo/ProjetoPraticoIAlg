9107c96d280e1c1ac126fa0aaac7ab7c32a4cdd6
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2023-11-19 19:54:01 -0300

    Checkpoint (Entrega parcial do trabalho)
    - Busca binária por ID no arquvo .DAT;
    - busca sequencial por nome no arquvo .DAT;
    - ordenação Shell Sort por nome;
    - importação do CSV para o arquivo .DAT;
    - inserção e listagem no arquvo .DAT.

--- projeto.cpp
+++ projeto.cpp
@@ -1,3 +1,8 @@
+/*
+Nomes: Gabriel Fagundes Mesquita Sousa, Gabriel Luis Moreira Castanhede, Bruno Souza Carvalho.
+Tema: Países.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -128,7 +133,121 @@ void shellSortName(fstream& arqBin, info*& pais, int cap){
 
     cout << "Ordenação eficiente por nome concluída." << endl;
 
+    arqBin.seekg(0, ios::beg);
+
     gravaBin(arqBin, pais, cap);
+}
+
+void buscaPorNome(info* pais, int cap, const char* nomeProcurado) {
+    bool encontrado = false;
+
+    for (int i = 0; i < cap; ++i) {
+        if (strcmp(pais[i].nome, nomeProcurado) == 0 and not encontrado) {
+            cout << "ID: " << pais[i].id << endl
+                 << "Nome: " << pais[i].nome << endl
+                 << "População: " << pais[i].pop << endl
+                 << "Idioma: " << pais[i].idioma << endl
+                 << "Descrição: " << pais[i].desc << endl;
+
+            encontrado = true;
+        }
+    }
+
+    if (not encontrado) {
+        cout << "País não listado." << endl;
+    }
+}
+
+bool buscaBinariaPorId(info* pais, int cap, int idBusca, info& resultado) {
+    int inicio = 0;
+    int fim = cap - 1;
+
+    while (inicio <= fim) {
+        int meio = (inicio + fim) / 2;
+
+        if (pais[meio].id == idBusca) {
+            resultado = pais[meio];
+            return true;
+        } else if (pais[meio].id < idBusca) {
+            inicio = meio + 1;
+        } else {
+            fim = meio - 1;
+        }
+    }
+
+    return false;
+}
+
+void menu(fstream& arqBin, info*& pais, int cap) {
+    int opcao;
+    do {
+        cout << "\nMenu:\n"
+             << "1. Listar todos os países\n"
+             << "2. Inserir país\n"
+             << "3. Buscar país\n"
+             << "4. Ordenar alfabeticamente\n"
+             << "0. Sair\n"
+             << "Escolha uma opção: ";
+        cin >> opcao;
+        cin.ignore();
+
+        switch (opcao) {
+        case 1:
+            leBin(arqBin, pais, cap);
+            printBin(arqBin, pais, cap);
+            break;
+        case 2:
+            insertBin(arqBin, pais, cap);
+            break;
+        case 3: {
+            cout << "1. Buscar por nome\n" << "2. Buscar por ID\n";
+            int resp;
+            cin >> resp;
+            cin.ignore();
+            if(resp == 1){
+            char nomeProcurado[50];
+            cout << "Digite o país a ser procurado: ";
+            cin.getline(nomeProcurado, sizeof(nomeProcurado));
+            for (char& c : nomeProcurado) {
+                c = toupper(c);
+            }
+            buscaPorNome(pais, cap, nomeProcurado);
+            break;
+            } else if (resp == 2){
+                int idBusca;
+                cout << "Digite o ID a ser buscado: ";
+                cin >> idBusca;
+
+                cin.ignore();
+
+                info resultado;
+                if (buscaBinariaPorId(pais, cap, idBusca, resultado)) {
+                    cout << "Registro encontrado:\n";
+                    cout << "ID: " << resultado.id << "\nNome: " << resultado.nome << "\nPopulação: "
+                         << resultado.pop << "\nIdioma: " << resultado.idioma << "\nDescrição: " << resultado.desc << "\n";
+                } else {
+                    cout << "Registro com ID " << idBusca << " não encontrado.\n";
+                }
+            buscaBinariaPorId(pais, cap, idBusca, resultado);
+            break;
+            } else{
+                cout << "Opção inválida." << endl;
+                break;
+            }
+        }
+        case 4:
+            shellSortName(arqBin, pais, cap);
+            cout << "Países ordenados por nome.\n";
+            printBin(arqBin, pais, cap);
+            break;
+        case 0:
+            cout << "Saindo do programa.\n";
+            break;
+        default:
+            cout << "Opção inválida. Tente novamente.\n";
+        }
+
+    } while (opcao != 0);
 }
 
 int main(){
@@ -149,11 +268,8 @@ int main(){
     info* pais = new info[cap];
 
     importCSV(arqCSV, arqBin, pais, cap);
-    insertBin(arqBin, pais, cap);
-    leBin(arqBin, pais, cap);
-    printBin(arqBin, pais, cap);
-    shellSortName(arqBin, pais, cap);
-    printBin(arqBin, pais, cap);
+    menu(arqBin, pais, cap);
+
     arqBin.close();
     arqCSV.close();
     return 0;
